5f384955ff4828073f4aec8f718ed8f1f443877f
Thorsten Otto <admin@tho-otto.de>
2018-04-07 12:22:32 +0200

    Update nf_shutdown

--- tos/nf_shutd.c
+++ tos/nf_shutd.c
@@ -2,15 +2,17 @@
 #include <mint/arch/nf_ops.h>
 
 
-void nf_shutdown(void)
+long nf_shutdown(int mode)
 {
-	struct nf_ops *nf_ops;
+	struct nf_ops *ops;
+	long res = 0;
 
-	if ((nf_ops = nf_init()) != NULL)
+	if ((ops = nf_init()) != NULL)
 	{
-		long shutdown_id = NF_GET_ID(nf_ops, NF_ID_SHUTDOWN);
+		long shutdown_id = NF_GET_ID(ops, NF_ID_SHUTDOWN);
 		
 		if (shutdown_id)
-        	nf_ops->call(shutdown_id | 0);
+        	res = ops->call(shutdown_id | mode);
 	}
+	return res;
 }
