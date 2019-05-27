16656ce5696eb470851a8cf3dd3c585f3cf98c08
Thorsten Otto <admin@tho-otto.de>
2019-05-27 04:03:55 +0200

    Update some copyright headers

--- win32/hv_prefs.c
+++ win32/hv_prefs.c
@@ -1,3 +1,24 @@
+/*
+ * HypView - (c)      - 2019 Thorsten Otto
+ *
+ * A replacement hypertext viewer
+ *
+ * This file is part of HypView.
+ *
+ * HypView is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HypView is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HypView; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "hv_defs.h"
 #include "hypdebug.h"
 #include "resource.rh"
